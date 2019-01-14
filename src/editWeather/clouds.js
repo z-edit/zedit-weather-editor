7d25f5737db67a1a0feb39f47a8136674e13432c
Colin <why101@msn.com>
2019-01-14 13:03:02 -0800

    created cloudLayerService

--- src/editWeather/clouds.js
+++ src/editWeather/clouds.js
@@ -1,54 +1,14 @@
-ngapp.controller('editWeatherCloudsController', function($scope) {
+ngapp.controller('editWeatherCloudsController', function($scope, cloudLayerService) {
     $scope.disabledLayers = [];
     $scope.cloudLayers = [];
 
-    let ctlChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';',
-        '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-        'I', 'J', 'K', 'L'];
-
-    let getColor = function(colorHandle, alphaHandle, path) {
-        let alpha = xelib.GetFloatValue(alphaHandle, path),
-            red = xelib.GetIntValue(colorHandle, `${path}\\Red`),
-            green = xelib.GetIntValue(colorHandle, `${path}\\Green`),
-            blue = xelib.GetIntValue(colorHandle, `${path}\\Blue`);
-        return new Color(`rgba(${red}, ${green}, ${blue}, ${alpha})`);
-    };
-
-    let buildLayer = function(disabledLayers, index, elements) {
-        let disabled = disabledLayers.includes(index),
-            a = disabled ? $scope.disabledLayers : $scope.cloudLayers,
-            ctlPath = index < ctlChars.length && `${ctlChars[index]}0TX`,
-            {colors, alphas, xSpeeds, ySpeeds} = elements,
-            alphaHandle = xelib.GetElement(alphas, `[${index}]`),
-            colorHandle = xelib.GetElement(colors, `[${index}]`);
-        a.push({
-            index,
-            disabled,
-            xSpeed: parseFloat(xelib.GetValue(xSpeeds, `[${index}]`)),
-            ySpeed: parseFloat(xelib.GetValue(ySpeeds, `[${index}]`)),
-            texture: ctlPath && xelib.GetValue($scope.handle, ctlPath),
-            sunriseColor: getColor(colorHandle, alphaHandle, 'Sunrise'),
-            dayColor: getColor(colorHandle, alphaHandle, 'Day'),
-            sunsetColor: getColor(colorHandle, alphaHandle, 'Sunset'),
-            nightColor: getColor(colorHandle, alphaHandle, 'Night')
-        });
-    };
-
     let loadLayers = function() {
-        let mapping = {
-            ySpeeds: 'Cloud Speed\\RNAM',
-            xSpeeds: 'Cloud Speed\\QNAM',
-            alphas: 'JNAM',
-            colors: 'PNAM'
-        };
-        let elements = {};
-        Object.keys(mapping).forEach(key => {
-            elements[key] = xelib.GetElement($scope.handle, mapping[key]);
+        xelib.WithHandleGroup(() => {
+            cloudLayerService.getLayers($scope.handle).forEach(layer => {
+                let key = layer.disabled ? 'disabledLayers' : 'cloudLayers';
+                $scope[key].push(layer);
+            });
         });
-        let disabledLayers = xelib.GetEnabledFlags($scope.handle, 'NAM1')
-            .map(str => parseInt(str, 10));
-        for (let i = 0; i < 32; i++)
-            buildLayer(disabledLayers, i, elements);
     };
 
     $scope.addLayer = function(item) {
