688731025014fae31251a5029eb7622940cf265e
Colin <why101@msn.com>
2019-01-14 17:12:25 -0800

    added mousewheel control to alpha input

--- src/alphaInput.js
+++ src/alphaInput.js
@@ -7,18 +7,43 @@ ngapp.directive('alphaInput', function() {
         template: '<input type="text" ng-model="alphaText" />',
         link: function(scope, element) {
             element[0].title = 'Alpha';
+            let inputElement = element[0].firstElementChild,
+                skip = false;
+
+            let notFocused = function() {
+                return document.activeElement !== inputElement;
+            };
+
+            let onWheel = function(e) {
+                if (e.deltaY === 0 || notFocused()) return;
+                e.preventDefault();
+                let offset = e.deltaY < 0 ? 1 : -1;
+                try {
+                    let f = parseFloat(scope.alphaText) * 20 + offset;
+                    f = Math.max(Math.min(f, 20), 0);
+                    scope.$applyAsync(() => {
+                        scope.alphaText = (Math.floor(f) / 20.0).toFixed(2);
+                    });
+                } catch (x) {}
+            };
+
+            element[0].addEventListener('wheel', onWheel);
 
             scope.$watch('color.channel.alpha', function() {
-                let alphaText = scope.color.getAlpha();
-                if (alphaText === scope.alphaText) return;
-                scope.alphaText = alphaText;
+                if (skip) return skip = false;
+                scope.alphaText = scope.color.getAlpha();
             });
 
             scope.$watch('alphaText', function() {
                 try {
-                    scope.color.setAlpha(parseFloat(scope.alphaText));
+                    scope.color.channel.alpha = parseFloat(scope.alphaText) * 255;
+                    skip = true;
                 } catch(x) {}
             });
+
+            scope.$on('$destroy', function() {
+                element[0].removeEventListener('wheel', onWheel);
+            })
         }
     }
 });
--- todos.md
+++ todos.md
@@ -1,16 +1,14 @@
 # Weather Editor Todos
 
-1. Update color when user changes alpha value
-2. Mouse wheel to adjust alpha value
-3. Mouse wheel to adjust X/Y speed
-4. Texture selector default options
-5. Color selector default options
-6. Fix color selector custom option display
-7. Add Layer dropdown maximum height
-8. Save cloud values to record
-9. Add colors tab
-10. Add dalc tab
-11. Add functions tab
+1. Mouse wheel to adjust X/Y speed
+2. Texture selector default options
+3. Color selector default options
+4. Fix color selector custom option display
+5. Add Layer dropdown maximum height
+6. Save cloud values to record
+7. Add colors tab
+8. Add dalc tab
+9. Add functions tab
     - Clear unused data option
     - Copy clouds from other weather
     - Copy Colors from other weather
