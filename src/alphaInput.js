183edc354651eb8088b8d11873e2627cb6ec7fa1
Colin <why101@msn.com>
2019-01-14 16:13:17 -0800

    fixed alpha input

--- src/alphaInput.js
+++ src/alphaInput.js
@@ -8,21 +8,13 @@ ngapp.directive('alphaInput', function() {
         link: function(scope, element) {
             element[0].title = 'Alpha';
 
-            let thaw = () => {
-                if (!scope.freeze) return;
-                scope.freeze = false;
-                return true;
-            };
-
-            scope.$watch('color', function() {
-                if (thaw()) return;
-                scope.freeze = true;
-                scope.alphaText = scope.color.getAlpha();
+            scope.$watch('color.channel.alpha', function() {
+                let alphaText = scope.color.getAlpha();
+                if (alphaText === scope.alphaText) return;
+                scope.alphaText = alphaText;
             });
 
             scope.$watch('alphaText', function() {
-                if (thaw()) return;
-                scope.freeze = true;
                 try {
                     scope.color.setAlpha(parseFloat(scope.alphaText));
                 } catch(x) {}
